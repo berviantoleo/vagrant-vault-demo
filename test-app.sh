e1074ee12dbf88fa3c852d129759a02cd6c6983c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-10-11 16:43:21 -0700

    Update test-app.sh

--- test-app.sh
+++ test-app.sh
@@ -1,12 +1,14 @@
+#!/bin/sh -ex
+
 IP_ADDRESS=192.168.56.4
 PORT=30008
 echo
 echo "Testing Root"
-curl "http://$IP_ADDRESS:$PORT/" -v
+curl --retry 5 --retry-connrefused "http://$IP_ADDRESS:$PORT/" -v
 echo
 echo "Testing Add Secret"
-curl -X 'POST' -d '{"Name": "test", "Data": {"test": "ok"}}' -H 'Content-Type: application/json' "http://$IP_ADDRESS:$PORT/secrets" -v
+curl --retry 5 --retry-connrefused -X 'POST' -d '{"Name": "test", "Data": {"test": "ok"}}' -H 'Content-Type: application/json' "http://$IP_ADDRESS:$PORT/secrets" -v
 echo
 echo "Testing Retrieve Secret"
-curl "http://$IP_ADDRESS:$PORT/secrets/test" -v
+curl --retry 5 --retry-connrefused "http://$IP_ADDRESS:$PORT/secrets/test" -v
 echo
